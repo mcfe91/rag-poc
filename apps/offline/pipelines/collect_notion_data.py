0ba22fb7fa35cb5dadf80371268675200bfe798a
Max McFerren <mmcferren91@gmail.com>
2025-05-09 10:30:08 -0400

    Add save to disk to collect notion data pipeline

--- apps/offline/pipelines/collect_notion_data.py
+++ apps/offline/pipelines/collect_notion_data.py
@@ -8,9 +8,18 @@ from steps.collect_notion_data import (
     extract_notion_documents
 )
 
+from steps.infrastructure import save_documents_to_disk
+
 @pipeline
 def collect_notion_data(database_ids: list[str], data_dir: Path, to_s3: bool = False) -> None:
+    notion_data_dir = data_dir / "notion"
+    notion_data_dir.mkdir(parents=True, exist_ok=True)
+
+    invocation_ids = []
     for index, database_id in enumerate(database_ids):
         logger.info(f"Collecting pages from database '{database_id}'")
         documents_metadata = extract_notion_documents_metadata(database_id=database_id)
         documents_data = extract_notion_documents(documents_metadata=documents_metadata)
+
+        result = save_documents_to_disk(documents=documents_data, output_dir=notion_data_dir / f"database_{index}")
+        invocation_ids.append(result.invocation_id)
--- apps/offline/src/offline/domain/document.py
+++ apps/offline/src/offline/domain/document.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 from pydantic import BaseModel, Field
 
 from offline import utils
@@ -16,3 +19,38 @@ class Document(BaseModel):
     content_quality_score: float | None = None
     summary: str | None = None
     child_urls: list[str] = Field(default_factory=list)
+
+    def write(
+        self, output_dir: Path, obfuscate: bool = False, also_save_as_txt: bool = False
+    ) -> None:
+        """Write document data to file, optionally obfuscating sensitive information.
+
+        Args:
+            output_dir: Directory path where the files should be written.
+            obfuscate: If True, sensitive information will be obfuscated.
+            also_save_as_txt: If True, content will also be saved as a text file.
+        """
+
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        if obfuscate:
+            self.obfuscate()
+        
+        json_page = self.model_dump()
+
+        output_file = output_dir / f"{self.id}.json"
+        with open(output_file, "w", encoding="utf-8") as f:
+            json.dump(
+                json_page,
+                f,
+                indent=4,
+                ensure_ascii=False,
+            )
+        
+        if also_save_as_txt:
+            txt_path = output_file.with_suffix(".txt")
+            with open(txt_path, "w", encoding="utf-8") as f:
+                f.write(self.content)
+
+    def obfuscate(self) -> "Document":
+        pass
